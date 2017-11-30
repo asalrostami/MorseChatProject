e5540ba57a30a9481a312fd23e5924db78f833be
asal <asal.rostami1984@gmail.com>
2017-11-30 13:18:58 -0500

    Added menu for logout

--- app/src/main/java/com/example/asal/morsechatproject/MainActivity.java
+++ app/src/main/java/com/example/asal/morsechatproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -35,15 +37,36 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
         if (currentUser == null) {
-            Intent intent = new Intent(MainActivity.this, StartPageActivity.class);
-            // doesn't allow the user to go to start page without login
-            // and doesn't allow the user go back to main activity
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+           LogoutUser();
         }
     }
 
+    private void LogoutUser() {
+        Intent intent = new Intent(MainActivity.this, StartPageActivity.class);
+        // doesn't allow the user to go to start page without login
+        // and doesn't allow the user go back to main activity
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
+    //for creating menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_menu,menu);
+         return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.main_logout_button)
+        {
+            mFirebaseAuth.signOut();
+
+            LogoutUser();
+        }
+        return true;
+    }
 }
