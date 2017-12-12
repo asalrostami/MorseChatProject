dd070dfbe53663edf1c7d4f0a985bb7b0e1c120c
asal <asal.rostami1984@gmail.com>
2017-12-12 12:42:32 -0500

    Set all user page

--- app/src/main/java/com/example/asal/morsechatproject/AllUsersActivity.java
+++ app/src/main/java/com/example/asal/morsechatproject/AllUsersActivity.java
@@ -1,24 +1,91 @@
 package com.example.asal.morsechatproject;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.asal.morsechatproject.Model.AllUsers;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AllUsersActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private RecyclerView mRecyclerView;
+    private DatabaseReference mDatabaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_users);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mToolbar = (Toolbar)findViewById(R.id.allUsers_app_bar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRecyclerView = (RecyclerView)findViewById(R.id.allUsers_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<AllUsers,AllUsersViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<AllUsers, AllUsersViewHolder>
+                (
+                        AllUsers.class,
+                        R.layout.all_users_display_layout,
+                        AllUsersViewHolder.class,
+                        mDatabaseReference
+                )
+        {
+            @Override
+            protected void populateViewHolder(AllUsersViewHolder viewHolder, AllUsers model, int position)
+            {
+
+                viewHolder.setUser_name(model.getUser_name());
+                viewHolder.setUser_status(model.getUser_status());
+                viewHolder.setUser_image(getApplicationContext(),model.getUser_image());
+            }
+        };
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class AllUsersViewHolder extends RecyclerView.ViewHolder
+    {
+        View mView;
+
+        public AllUsersViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setUser_name(String user_name) {
+            TextView userName = (TextView)mView.findViewById(R.id.tv_allUsers_userName);
+            userName.setText(user_name);
+        }
+        public void setUser_status(String user_status) {
+            TextView status = (TextView)mView.findViewById(R.id.tv_allUsers_status);
+            status.setText(user_status);
+        }
+        public void setUser_image(Context context,String user_image) {
+            CircleImageView image = (CircleImageView)mView.findViewById(R.id.allUsers_profile_image);
+            Picasso.with(context).load(user_image).resize(600, 600)
+                    .centerCrop().into(image);
+        }
     }
 }
