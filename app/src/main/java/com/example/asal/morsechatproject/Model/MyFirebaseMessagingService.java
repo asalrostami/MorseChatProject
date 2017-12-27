c0d1863224157bf059567a601ef3eb93669dd7c0
asal <asal.rostami1984@gmail.com>
2017-12-26 20:46:10 -0500

    set click on notification

--- app/src/main/java/com/example/asal/morsechatproject/Model/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/asal/morsechatproject/Model/MyFirebaseMessagingService.java
@@ -5,6 +5,8 @@ package com.example.asal.morsechatproject.Model;
 
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.example.asal.morsechatproject.R;
@@ -21,10 +23,36 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+
+        String notification_title = remoteMessage.getNotification().getTitle();
+        String notification_body = remoteMessage.getNotification().getBody();
+
+        String click_action = remoteMessage.getNotification().getClickAction();
+        String from_sender_id = remoteMessage.getData().get("from_sender_id").toString();
+
+
+
+
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.label1)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World!");
+                        .setContentTitle(notification_title)
+                        .setContentText(notification_body);
+
+
+
+        Intent resultIntent = new Intent(click_action);
+        resultIntent.putExtra("visit_user_id",from_sender_id);
+
+        // Because clicking the notification opens a new ("special") activity, there's
+        // no need to create an artificial back stack.
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
 
 
         // Sets an Random ID for the notification
